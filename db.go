d74a904651c9ce402cbc8ddba60756481c36b401
LucKer431 <egor70985@gmail.com>
2024-10-19 02:35:24 +0300

    Подогнал зависимости и пакеты, а так же настроил соединение с db

---
+++ db.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	//"log"
+
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Db       string `json:"db"`
+}
+
+func LoadConfig(filename string) (Config, error) {
+	var config Config
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return config, err
+	}
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return config, err
+	}
+	return config, nil
+}
+
+func ConnectToDB(config Config) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		config.Host, config.Port, config.User, config.Password, config.Db)
+	return sql.Open("postgres", connStr)
+}
